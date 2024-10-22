582a51864ea368436b3a4f9af891955cfd774ccc
ZeroTomato01 <146572680+ZeroTomato01@users.noreply.github.com>
2024-10-22 14:53:35 +0200

    deleted superfluous get method

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -105,27 +105,3 @@ namespace TheatreProject.Controllers
         }
     }
 }
-
-    public async Task<IActionResult> GetReservation([FromQuery] int id)
-    {
-        Reservation? DBReservation = await _reservationService.GetReservation(id);
-        if(DBReservation is not null)
-        {
-            return new OkObjectResult(DBReservation);
-        }
-        return new BadRequestObjectResult($"no threatre with given id: {id} was found in database");
-    }
-    protected async Task<IActionResult> PostReservation([FromBody] Reservation reservation)
-    {
-        return await _reservationService.PostReservation(reservation);
-    }
-    public async Task<IActionResult> UpdateReservation([FromBody] Reservation reservation)
-    {
-        return await _reservationService.UpdateReservation(reservation);
-    }
-    public async Task<IActionResult> DeleteReservation([FromQuery] int id)
-    {  
-        return await _reservationService.DeleteReservation(id);
-    }
-
-}
